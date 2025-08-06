06a407ad4835186d4b2d77eacc972bdfa0a3a8fe
baleradaniel <danielbalera021@gmail.com>
2025-08-05 21:57:20 -0300

    commit

---
+++ db_images/project_image02/index.php
@@ -0,0 +1,64 @@
+<?php
+// Incluir o arquivo de conexão
+require_once 'conecta.php';
+
+// Cria a consulta SQL para selecionar os dados principais (sem a coluna de imagem)
+$querySelecao = "SELECT codigo, evento, descricao, nome_imagem, tamanho_imagem FROM tabela_imagem";
+
+$result = mysqli_query($conn, $querySelecao);
+
+if (!$result) {
+    die("Erro na consulta: " . mysqli_error($conn));
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Armazenamento de Imagens no BD MySQL</title>
+</head>
+<body>
+    <h2>Selecione um novo arquivo de imagem</h2>
+    <form action="upload.php" enctype="multipart/form-data" method="post">
+        <input type="hidden" name="MAX_FILE_SIZE" value="999999999">
+
+        <input type="text" name="evento" placeholder="Nome do Evento" >
+
+        <input type="text" name="descricao" placeholder="Descrição do Evento">
+
+        <input type="file" name="imagem">
+
+        <input type="submit" value="Enviar Imagem">
+    </form>
+
+    <table border="1">
+    <tr>
+        <td align="center">Código</td>
+        <td align="center">Evento</td>
+        <td align="center">Descrição</td>
+        <td align="center">Nome da Imagem</td>
+        <td align="center">Tamanho</td>
+        <td align="center">Vizualizar Imagem</td>
+        <td align="center">Excluir Imagem</td>
+    </tr>
+    <?php
+    while ($arquivos = mysqli_fetch_array($result)) {
+        ?>
+            <tr>
+                <td align="center"><?php echo $arquivos['codigo']; ?></td>
+                <td align="center"><?php echo $arquivos['evento']; ?></td>
+                <td align="center"><?php echo $arquivos['descricao']; ?></td>
+                <td align="center"><?php echo $arquivos['nome_imagem']; ?></td>
+                <td align="center"><?php echo $arquivos['tamanho_imagem']; ?> bytes</td>
+                <td align="center">
+                    <a href="ver_imagens.php?id=<?php echo $arquivos['codigo'];?>">Vizualizar</a>
+                    <td align="center">
+                        <a href="excluir_imagem.php?codigo=<?php echo $arquivos['codigo']; ?>">Excluir</a>
+                    </td>
+                </tr>
+        <?php    } ?>
+        </table>
+</body>
+</html>
---
+++ db_images/project_image02/upload.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'conecta.php';
+
+// Obtem os dados do formulário
+$evento = $_POST['evento'];
+$descricao = $_POST['descricao'];
+$imagem = $_FILES['imagem']['tmp_name'];
+$tamanho = $_FILES['imagem']['size'];
+$tipo = $_FILES['imagem']['type'];
+$nome = $_FILES['imagem']['name'];
+
+// Verifica se o arquivo foi enviado
+if(!empty($imagem) && !empty($tamanho >0)){
+    // Lê o conteúdo do arquivo 
+    $fp = fopen($imagem,'rb');
+    $conteudo = fread($fp, filesize($imagem));
+    fclose($fp);
+
+    // Protege contra problemas de caracteres no mysql
+    $conteudo = mysqli_real_escape_string($conn, $conteudo);
+
+    // Insere os dados no banco de dados
+    $queryInsercao = "INSERT INTO tabela_imagem (evento, descricao, nome_imagem, tamanho_imagem, tipo_imagem, imagem) 
+                      VALUES ('$evento', '$descricao', '$nome', '$tamanho', '$tipo', '$conteudo')";
+    $result = mysqli_query($conn, $queryInsercao);
+
+    // Verifica se a inserção foi bem-sucedida
+    if($result){
+        echo "Imagem enviada com sucesso!";
+        header("Location: index.php"); // Redireciona para a página principal após o envio
+    } else {
+        die ("Erro ao enviar imagem: " . mysqli_error($conn));
+    }
+} else {
+    echo "Nenhum arquivo enviado ou o tamanho do arquivo é inválido.";
+}
+?>
